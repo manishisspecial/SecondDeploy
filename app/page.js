d80d40254cc68d14de90e2f1b4493a880ad34666
manishisspecial <157202726+manishisspecial@users.noreply.github.com>
2024-12-08 20:59:00 +0530

    Upload 06

--- app/page.js
+++ app/page.js
@@ -2,28 +2,29 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 style={{ color: "#4CAF50" }}>Welcome to My React Page!</h1>
-        <h2 style={{ color: "#555" }}>A Special Thanks to you "Satyam Sir"</h2>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-2 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="p-5 border border-2 border-indigo-600 text-2xl flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-yellow-600 font-bold">Welcome to My React Page!</h1>
+        <h2 className="text-teal-600 font-medium">A Special Thanks to you Sir</h2>
         <Image
-          className="dark:invert"
+          className="dark:invert shadow-2xl"
           src="/new.jpg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
-        <p style={{ fontSize: "18px", lineHeight: "1.6", color: "#333" }}>
+        <p className="text-green-600">
           Dear Sir,
-          <br />
+        </p>
+        <p className="text-violet-600">
           Thank you for inspiring me to learn and grow. This is my first React
           project, and I dedicate it to you!
         </p>
-      </main>
-      <footer style={{ marginTop: "20px", fontSize: "14px", color: "#888" }}>
+      <footer className="font-black">
         Built with ❤️ using React
       </footer>
+      </main>
     </div>
   );
 }
